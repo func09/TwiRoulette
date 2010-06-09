d309eda0d3804146302a58a52520d18a670c73d4
func09 <mitsuru.haga@gmail.com>
2010-06-09 12:59:03 +0900

    Matching#uidを初期値でランダムな文字列を設定するようにした

--- app/models/matching.rb
+++ app/models/matching.rb
@@ -1,5 +1,14 @@
+require 'forgery'
 class Matching < ActiveRecord::Base
+  
+  UID_LENGTH = 6
+  
   has_friendly_id :uid
   has_many :messages
   has_many :users, :through => :messages
+  
+  default_value_for :uid do
+    Forgery(:basic).text(:at_least => UID_LENGTH, :at_most => UID_LENGTH)
+  end
+
 end
