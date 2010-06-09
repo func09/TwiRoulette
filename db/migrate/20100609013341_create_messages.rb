31eb701864aad5c87a5e51509bd83f60f9102f36
func09 <mitsuru.haga@gmail.com>
2010-06-09 12:38:12 +0900

    messagesとmatchingsのテーブル構造を修正

--- db/migrate/20100609013341_create_messages.rb
+++ db/migrate/20100609013341_create_messages.rb
@@ -3,6 +3,9 @@ class CreateMessages < ActiveRecord::Migration
     create_table :messages do |t|
       t.integer :user_id, :null => false
       t.string :body, :null => false
+      t.integer :matching_id
+      t.boolean :matched
+      t.datetime :matched_at
       t.timestamps
     end
     add_index :messages, [:user_id]
--- db/migrate/20100609013349_create_matchings.rb
+++ db/migrate/20100609013349_create_matchings.rb
@@ -2,11 +2,6 @@ class CreateMatchings < ActiveRecord::Migration
   def self.up
     create_table :matchings do |t|
       t.string :uid, :null => false
-      t.integer :user_a_id, :null => false
-      t.integer :user_a_message_id, :null => false
-      t.integer :user_b_id, :null => false
-      t.integer :user_b_message_id, :null => false
-      # TODO: screen_nameは変更になる可能性があるので注意
       t.timestamps
     end
     add_index :matchings, [:uid], :unique => true
