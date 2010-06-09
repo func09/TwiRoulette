a3f3aed0e193c3d8117c4e090631511b5e093c29
func09 <mitsuru.haga@gmail.com>
2010-06-10 02:07:20 +0900

    Message.match_allメソッドの実装

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,32 @@ class Message < ActiveRecord::Base
   
   named_scope :yet, :conditions => ['matched = ?', false]
   
+  class << self
+    
+    # マッチングする
+    def match_all
+      logger.info "Start matching #{yet.count} messages.(#{Time.now})"
+      arr = yet
+      until arr.empty?
+        a = arr.slice!(rand(arr.size - 1))
+        b = arr.slice!(rand(arr.size - 1))
+        match = Matching.create
+        transaction do
+          a.matching = match
+          a.matched = true
+          a.matched_at = Time.now
+          a.save
+          b.matching = match
+          b.matched = true
+          b.matched_at = Time.now
+          b.save
+        end
+      end
+      logger.info "Finished matching.(#{Time.now})"
+    end
+    
+  end
+  
 end
 
 # == Schema Information
