3ffca2c5e2a4a0e6f26153a4b8405335874e0347
func09 <mitsuru.haga@gmail.com>
2010-06-09 13:01:19 +0900

    Install Configatron

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,9 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'friendly_id'
+  config.gem 'twitter-auth', :lib => 'twitter_auth'
+  config.gem 'forgery', :source => 'http://gemcutter.org'
+  config.gem 'configatron'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
